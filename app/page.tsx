7fa7a0d2742787e8ab25205c75021e592ac3c141
ShiRin <2463951865z@gmail.com>
2026-01-06 20:47:36 +0800

    database

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,24 @@
+"use client";
+import { Button } from "@/components/ui/button";
+import { api } from "@/convex/_generated/api";
+import { useMutation, useQuery } from "convex/react";
+
 export default function Home() {
-  return <h1>Page</h1>;
+  const projects = useQuery(api.projects.get);
+  const createProject = useMutation(api.projects.create);
+  return (
+    <div>
+      <h1>Projects</h1>
+      <ul>
+        {projects?.map((project) => (
+          <li key={project._id}>
+            {project.name} {project.ownerId}
+          </li>
+        ))}
+      </ul>
+      <Button onClick={() => createProject({ name: "New Project" })}>
+        Create Project
+      </Button>
+    </div>
+  );
 }
---
+++ components/providers.tsx
@@ -0,0 +1,42 @@
+"use client";
+import { useAuth, UserButton } from "@clerk/nextjs";
+import { ConvexProviderWithClerk } from "convex/react-clerk";
+import { ClerkProvider } from "@clerk/nextjs";
+import {
+  Authenticated,
+  AuthLoading,
+  ConvexReactClient,
+  Unauthenticated,
+} from "convex/react";
+import { dark } from "@clerk/themes";
+import { ThemeProvider } from "next-themes";
+import UnauthenticatedView from "@/features/auth/components/unauthenticated-view";
+
+if (!process.env.NEXT_PUBLIC_CONVEX_URL) {
+  throw new Error("Missing NEXT_PUBLIC_CONVEX_URL in your .env file");
+}
+
+const convex = new ConvexReactClient(process.env.NEXT_PUBLIC_CONVEX_URL);
+export default function Providers({ children }: { children: React.ReactNode }) {
+  return (
+    <ThemeProvider
+      attribute="class"
+      defaultTheme="dark"
+      enableSystem
+      disableTransitionOnChange
+    >
+      <ClerkProvider appearance={{ theme: dark }}>
+        <ConvexProviderWithClerk client={convex} useAuth={useAuth}>
+          <Authenticated>
+            <UserButton />
+            {children}
+          </Authenticated>
+          <Unauthenticated>
+            <UnauthenticatedView />
+          </Unauthenticated>
+          <AuthLoading>Authloading...</AuthLoading>
+        </ConvexProviderWithClerk>
+      </ClerkProvider>
+    </ThemeProvider>
+  );
+}
